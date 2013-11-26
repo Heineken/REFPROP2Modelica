811818801ea05425bd697842bc9fcc640523a5b7
jowr <jowr@mek.dtu.dk>
2013-11-26 14:40:38 +0100

    New test routine

--- _wrapper/cpptest/cpp_wrapped.cpp
+++ _wrapper/cpptest/cpp_wrapped.cpp
@@ -3,21 +3,11 @@
 #include <string.h>
 #include <iomanip>
 #include <sstream>
-#include "../src/refprop_wrapper.h"
-#include "../src/refprop_library.h"
 #include <math.h>
 #include <ctime>
-
-// Some constants for REFPROP... defined by macros for ease of use
-#define refpropcharlength 255
-#define refpropcharlong 10000
-#define filepathlength 255
-#define lengthofreference 3
-#define errormessagelength 255
-#define ncmax 20		// Note: ncmax is the max number of components
-#define numparams 72
-#define maxcoefs 50
-
+#include "../src/refprop_library.h"
+#include "../src/refprop_wrapper.h"
+#include "cpp_wrapped.h"
 
 int main(int argc, char* argv[]){
 	char errormsg[255];
@@ -26,23 +16,6 @@ int main(int argc, char* argv[]){
 	double *ders;
 	double *trns;
 	int DEBUG = 0;
-
-
-	/* Define some new functions to access low-level refprop routines.
-	 *
-	 * The new functions should be used to compare the speed of the wrapper to the speed
-	 * of the Fortran code and the Matlab implementation.
-	 */
-
-//#	if defined(_WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(__WIN64__)
-//#  		define __ISWINDOWS__
-//#  		include <windows.h>
-//#  		define _CRT_SECURE_NO_WARNINGS
-//#	elif __APPLE__
-//#  		define __ISAPPLE__
-//#	elif __linux
-//#  		define __ISLINUX__
-//#	endif
 
 	int nX=2; // Number of components
 
@@ -64,23 +37,23 @@ int main(int argc, char* argv[]){
 	x[0] = 0.2;
 	x[1] = 1.0 - x[0];
 
-	double p  = 50e5;
-    double h  = 3e5;
-	double dh = 25e5;
+	double p  = 50.e5;
+    double h  = 3.0e5;
+	double dh = 25e1;
 
-	int N     = 75; // steps in enthalpy
-	int M     = 15; // repetitions
+	int N     = 390; // steps in enthalpy
+	int M     = 253; // repetitions
 
 	double res  = 0.0;
 	double h_in = 0.0;
 
 	// Format the output properly
-	int decimals = 4;
-	int width    = 10;
-	char buffer [50];
+	char buffer [100];
+	int limit    = 10;
 
 
 	std::ostringstream out;
+	out << std::endl;
 
 	// Get the time, calculate and get the time again
 	time_t tstart, tend;
@@ -89,103 +62,101 @@ int main(int argc, char* argv[]){
 		for (int count = 0; count < N; count++) {
 			h_in = h + dh * count / (N - 1);
 			res = props_REFPROP((char*)"u", (char*)"ph", fluidname, ders, trns, props, p, h_in, x, 0, thepathChar, errormsg, DEBUG);
-			sprintf (buffer, "d = %8.4f [kg/m3]",props[4]);
-			out << buffer << std::endl;
-			sprintf (buffer, "T = %8.4f [K]",props[2]);
-			out << buffer << std::endl << std::endl;
+			if (N*M<limit) {
+				sprintf (buffer, "d = %8.4f [kg/m3]",props[4]);
+				out << buffer << std::endl;
+				sprintf (buffer, "T = %8.4f [K]",props[2]);
+				out << buffer << std::endl << std::endl;
+			}
 		}
 	}
 	tend = time(0);
 
-	if (N*M<10) {
-		printf("%s", out.str().c_str());
+	if (N*M<limit) {
+		//printf("%s", out.str().c_str());
 	} else {
-		printf("\nThere were %d calls to the wrapper. \n", N*M);
+		sprintf(buffer, "\nThere were %d calls to the wrapper.", N*M);
+		out << buffer << std::endl;
 	}
 	int deltat = difftime(tend, tstart);
-	printf("It took %d second(s) with an average time per call of %8.4f ms.\n",deltat,deltat*1e3/N/M);
+	sprintf(buffer,"It took %d second(s) with an average time per call of %8.4f ms.",deltat,deltat*1e3/N/M);
+	out << buffer << std::endl << std::endl << std::endl;
 
 
 	// Do the same for the unwrapped functions
+	char hf[refpropcharlength*ncmax], hrf[lengthofreference+1],
+	      herr[errormessagelength+1],hfmix[refpropcharlength+1];
 
-//	char hf[refpropcharlength*ncmax], hrf[lengthofreference+1],
-//	      herr[errormessagelength+1],hfmix[refpropcharlength+1];
-//
-//	double xliq[ncmax],xvap[ncmax],f[ncmax];
-//	double wm,ttp,tnbp,tc,pc,dc,zc,acf,dip,rgas;
-//	double t=100.0;
-//	double dl,dv;
-//	long j=1 ,lerr = 0, i=0, ierr=0;
-//	double d=0.0,q=0.0,e=0.0,s=0.0,cv=0.0,cp=0.0,w=0.0,b=0.0,c=0.0,
-//		  dpdrho,d2pdd2,dpdt,dhdt_d,dhdt_p,dhdp_t,dhdp_d,
-//		  sigma,dhdd_t,dhdd_p,eta,tcx,pp,tt,hjt,h1,dd;
-//
-//	strcpy(hf,thepathChar);
-//	strcat(hf,theSepChar);
-//	strcat(hf,"ammoniaL.fld|");
-//	strcat(hf,thepathChar);
-//	strcat(hf,theSepChar);
-//	strcat(hf,"water.fld");
-//
-//	strcpy(hfmix,thepathChar);
-//	strcat(hfmix,theSepChar);
-//	strcat(hfmix,"fluids");
-//	strcat(hfmix,theSepChar);
-//	strcat(hfmix,"hmx.bnc");
-//
-//	strcpy(hrf,"DEF");
-//	strcpy(herr,"Ok");
-//
-//
-////	SETUPdll_POINTER SETUPdll;
-////	WMOLdll_POINTER WMOLdll;
-////	PHFLSHdll_POINTER  PHFLSHdll;
-//
-//	//...Call SETUP to initialize the program
-//	//long ,char*,char*,char*,long ,char*,long ,long ,long ,long
-//	SETUPdll(i, hf, hfmix, hrf, ierr, herr);//,
-//				//refpropcharlength*ncmax,refpropcharlength,
-//				//lengthofreference,errormessagelength);
-//
-//	if (ierr != 0) printf("%s\n",herr);
-//
-//	WMOLdll(x,wm);
-//	printf("WM, %10.4f\n",wm);
-//
-//	double hmol  = h*wm/1000.0;
-//	double dhmol = dh*wm/1000.0;
-//	double pkpa  = p/1000.0;
-//
-//	//printf("hmol, pkpa %10.4f, %10.4f\n",hmol,pkpa);
-//
-//	//PHFLSHdll (&pkpa,&hmol,x,&tt,&dd,&dl,&dv,xliq,xvap,&q,&e,&s,&cv,&cp,&w,&ierr,herr,errormessagelength);
-//    //printf("T, D, Pkpa, hmol from PHFLSH   %10.4f,%10.4f,%10.4f,%10.4f\n",tt,dd,pkpa,hmol);
-//
-//
-//
-//
-//	tstart = time(0);
-//	for (int i = 0; i < M; i++) {
-//		for (int count = 0; count < N; count++) {
-//			h_in = hmol + dhmol * count / (N - 1);
-//			PHFLSHdll(pkpa,h_in,x,tt,dd,dl,dv,xliq,xvap,q,e,s,cv,cp,w,ierr,herr,errormessagelength);
-//			sprintf (buffer, "d = %8.4f [kg/m3]",dd);
-//			out << buffer << std::endl;
-//			sprintf (buffer, "T = %8.4f [K]",tt);
-//			out << buffer << std::endl << std::endl;
-//		}
-//	}
-//	tend = time(0);
-//
-//	if (N*M<10) {
-//		printf("%s", out.str().c_str());
-//	} else {
-//		printf("\nThere were %d calls to REFPROP. \n", N*M);
-//	}
-//
-//	deltat = difftime(tend, tstart);
-//	printf("It took %d second(s) with an average time per call of %8.4f ms.\n",deltat,deltat*1e3/N/M);
+	double xmol[ncmax],xliq[ncmax],xvap[ncmax],f[ncmax];
+	double wm,ttp,tnbp,tc,pc,dc,zc,acf,dip,rgas;
+	double t=100.0;
+	double dl,dv;
+	long j=1 ,lerr = 0, i=0, ierr=0;
+	double d=0.0,q=0.0,e=0.0,s=0.0,cv=0.0,cp=0.0,w=0.0,b=0.0,c=0.0,
+		  dpdrho,d2pdd2,dpdt,dhdt_d,dhdt_p,dhdp_t,dhdp_d,
+		  sigma,dhdd_t,dhdd_p,eta,tcx,pp,tt,hjt,h1,dd;
 
-	printf("%s\n"," ");
+	strcpy(hf,thepathChar);
+	strcat(hf,theSepChar);
+	strcat(hf,"fluids");
+	strcat(hf,theSepChar);
+	strcat(hf,"ammoniaL.fld|");
+	strcat(hf,thepathChar);
+	strcat(hf,theSepChar);
+	strcat(hf,"fluids");
+	strcat(hf,theSepChar);
+	strcat(hf,"water.fld");
+
+	strcpy(hfmix,thepathChar);
+	strcat(hfmix,theSepChar);
+	strcat(hfmix,"fluids");
+	strcat(hfmix,theSepChar);
+	strcat(hfmix,"HMX.BNC");
+
+	strcpy(hrf,"DEF");
+	strcpy(herr,"Ok");
+
+	i = nX;
+
+	//...Call SETUP to initialize the program
+	//long ,char*,char*,char*,long ,char*,long ,long ,long ,long
+	SETUPdll(i, hf, hfmix, hrf, ierr, herr,refpropcharlength*ncmax,refpropcharlength,lengthofreference,errormessagelength);
+
+	if (ierr != 0) printf("%s\n",herr);
+
+	//WMOLdll(x,wm);
+	XMOLEdll(x,xmol,wm);
+
+	double hmol  = h*wm/1000.0;
+	double dhmol = dh*wm/1000.0;
+	double pkpa  = p/1000.0;
+
+	tstart = time(0);
+	for (int i = 0; i < M; i++) {
+		for (int count = 0; count < N; count++) {
+			h_in = hmol + dhmol * count / (N - 1);
+			PHFLSHdll(pkpa,h_in,xmol,tt,dd,dl,dv,xliq,xvap,q,e,s,cv,cp,w,ierr,herr,errormessagelength);
+			if (N*M<limit) {
+				sprintf (buffer, "d = %8.4f [kg/m3]",dd*wm);
+				out << buffer << std::endl;
+				sprintf (buffer, "T = %8.4f [K]",tt);
+				out << buffer << std::endl << std::endl;
+			}
+		}
+	}
+	tend = time(0);
+
+	if (N*M<limit) {
+		//printf("%s", out.str().c_str());
+	} else {
+		sprintf(buffer, "\nThere were %d calls to REFPROP.", N*M);
+		out << buffer << std::endl;
+	}
+
+	deltat = difftime(tend, tstart);
+	sprintf(buffer, "It took %d second(s) with an average time per call of %8.4f ms.",deltat,deltat*1e3/N/M);
+	out << buffer << std::endl << std::endl << std::endl;
+
+	printf("%s",out.str().c_str());
 	return 0;
 }
---
+++ _wrapper/cpptest/cpp_wrapped.h
@@ -0,0 +1,62 @@
+
+
+#ifndef CPP_WRAPPED_H
+#define CPP_WRAPPED_H
+
+#include "../src/refprop_library.h"
+
+// Some constants for REFPROP... defined by macros for ease of use
+#define refpropcharlength 255
+#define refpropcharlong 10000
+#define filepathlength 255
+#define lengthofreference 3
+#define errormessagelength 255
+#define ncmax 20		// Note: ncmax is the max number of components
+#define numparams 72
+#define maxcoefs 50
+
+/* Define some new functions to access low-level refprop routines.
+ *
+ * The new functions should be used to compare the speed of the wrapper to the speed
+ * of the Fortran code and the Matlab implementation.
+ */
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(_WIN64) || defined(__WIN64__)
+#	define __ISWINDOWS__
+#	include <windows.h>
+#	define _CRT_SECURE_NO_WARNINGS
+#elif __APPLE__
+#	define __ISAPPLE__
+#elif __linux
+#	define __ISLINUX__
+#endif
+
+#if defined(__ISWINDOWS__)
+#  	define CALLCONV __stdcall
+# 	define SETUPdll SETUPdll
+# 	define WMOLdll WMOLdll
+#	define XMOLEdll XMOLEdll
+# 	define PHFLSHdll PHFLSHdll
+#elif defined(__ISLINUX__)
+#	define CALLCONV
+#	define SETUPdll setupdll_
+#	define WMOLdll wmoldll_
+#	define XMOLEdll xmoledll_
+#	define PHFLSHdll phflshdll_
+#endif
+
+typedef void (CALLCONV *SETUPdll_POINTER)(long &,char*,char*,char*,long &,char*,long ,long ,long ,long );
+typedef void (CALLCONV *WMOLdll_POINTER)(double *,double &);
+typedef void (CALLCONV *XMOLEdll_POINTER)(double *,double *,double &);
+typedef void (CALLCONV *PHFLSHdll_POINTER)(double &,double &,double *,double &,double &,double &,double &,double *,double *,double &,double &,double &,double &,double &,double &,long &,char*,long );
+
+//SETUPdll_POINTER SETUPdll;
+//WMOLdll_POINTER WMOLdll;
+//PHFLSHdll_POINTER  PHFLSHdll;
+
+extern SETUPdll_POINTER SETUPdll;
+extern WMOLdll_POINTER WMOLdll;
+extern XMOLEdll_POINTER XMOLEdll;
+extern PHFLSHdll_POINTER  PHFLSHdll;
+
+#endif
